f2ba72ff00cc628eca29a33f0503b6ca028827b3
estebanzeb <estebanzeb@gmail.com>
2021-04-17 17:12:13 -0500

    Reto

--- arreglos/app.js
+++ arreglos/app.js
@@ -29,23 +29,78 @@ let hoteles=[
 let resultado;
 //Esta recorriendo la variable HOTELES
 //BUSCAR VARIOS
-/*resultado = hoteles.filter(function(hotel){
-    return (hotel.region=="caribe");
+
+/*resultado = hoteles.filter(function(hotel){       
+    return (hotel.region=="andina");
 });
 console.log(resultado);*/
 
 
 //Esta recorriendo la variable HOTELES
-//BUSCA UNO EN ESPECIFICO
-resultado = hoteles.find(function(hotel){
+//BUSCA UNO EN ESPECIFIC
+/*resultado = hoteles.find(function(hotel){
     return (hotel.id=="hot005");
+   
 });
 
-console.log(resultado);
+console.log(resultado);*/
 
+//BUSCAR SI ESTA, SOLO DEVUELVE (TRUE , FALSE)
+/*resultado = hoteles.some(function(hotel){
+    return (hotel.id=="558");
+});
+
+console.log(resultado);*/
+
+//BUSCA Y ACUMULA PARA PORDER SUMAR
+
+/*resultado = hoteles.reduce(function(acumulador,hotel){
+    return (hotel.estrellas+acumulador);
+},0);
+
+console.log(resultado)*/
 
 //Esta recorriendo la variable HOTELES
 /*let resultado = hoteles.filter((hotel)=>hotel.region=="caribe");
 
 console.log(resultado);*/
 
+let departamentos=Array(
+    {nombre:"Antioquia",capital:"Medellín",municipio:125,region:"Andina"},
+    {nombre:"Amazonas",capital:"Leticia",municipio:2,region:"Amazonica"},
+    {nombre:"Atlantico",capital:"Barranquilla",municipio:23,region:"Caribe"},
+    {nombre:"Caldas",capital:"Manizales",municipio:27,region:"Andina"},
+    {nombre:"Casanare",capital:"Yopal",municipio:19,region:"Orinoquía"},
+    {nombre:"Caquetá",capital:"Florencia",municipio:16,region:"Amazonica"}
+);
+
+resultado = departamentos.filter(function(departamento){       
+    return (departamento.region=="Andina");
+});
+console.log(resultado);
+
+
+resultado = departamentos.find(function(departamento){       
+    return (departamento.municipio=="27");
+});
+console.log(resultado);
+
+resultado = departamentos.filter(function(departamento){       
+    return (departamento.municipio>"20");
+});
+console.log(resultado);
+
+resultado = departamentos.some(function(departamento){       
+    return (departamento.capital=="Yopal");
+});
+console.log(resultado);
+
+resultado = departamentos.reduce(function(acumulador,departamento){
+    return (departamento.municipio+acumulador);
+},0); //el numero desde donde empieza el contador
+
+console.log(resultado)
+
+let resultado = hoteles.filter((hotel)=>hotel.region=="caribe");
+
+console.log(resultado);
